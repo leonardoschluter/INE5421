8ea745d03c25f85886539129c20caae4780166cd
Leonardo Schlüter Leite <leonardoschluter@gmail.com>
2018-11-02 19:03:57 -0300

    adicionando gitignore

--- src/compilador/Main.java
+++ src/compilador/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.print("Hello World !");
         AutomatoScanner scanner = new AutomatoScanner();
         try {
-            Automato automato = scanner.readFileTxt("/Users/leonardoschluter/Documents/T2_formais/T2_3-Formais/src/compilador/automato.txt");
+            Automato automato = scanner.readFileTxt("/home/leonardo/Documents/formais/T2_3-Formais/src/compilador/automato.txt");
         }catch (Exception e){
             e.printStackTrace();
         }
